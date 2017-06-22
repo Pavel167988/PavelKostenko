63a36b40abdc420582d906bd5adb6d47ecaf9bf8
Pavel <pasha.167988@yandex.by>
2017-06-22 20:00:07 +0300

    bootstrap

---
+++ index.php
@@ -0,0 +1,46 @@
+﻿<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Project1</title>
+</head>
+<body>
+    <div class="wrapper">
+         <header>
+             <div class="logo">
+
+             </div>
+             <div class="name">
+
+             </div>
+             <div class="date">
+
+             </div>
+             <div class="login">
+
+             </div>
+             <div class="password">
+
+             </div>
+         </header>
+            <nav class="nav_menu">
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+            </nav>
+             <div class="sidebar">
+                 <a href=""></a>
+                 <a href=""></a>
+                 <a href=""></a>
+                 <a href=""></a>
+                 <a href=""></a>
+                 <a href=""></a>
+                 <a href=""></a>
+             </div>
+               <div class="main"></div>
+             <footer></footer>
+    </div>
+
+</body>
+</html>
