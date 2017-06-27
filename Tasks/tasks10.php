d3ed5184f9eec39b1146e275b053792483308f3e
Pavel <pasha.167988@yandex.by>
2017-06-27 15:24:08 +0300

    update zadachi

--- Tasks/tasks10.php
+++ Tasks/tasks10.php
@@ -1,3 +1,24 @@
 <?php
+function arr_vivod ($arr){
+    if (is_array($arr)){
+        arr_vivod2($arr);
+    }
+    else {
+        echo "Eto ne massiv!";
+    }
+    return $arr;
 
+}
+function arr_vivod2 ($arr){
+        echo "<pre>";
+        print_r($arr);
+        echo "</pre>";
+    }
+
+
+
+
+    arr_vivod(array ("Home" , "Page" , "Insert"));
+
+//Задача пока без 3 функции(табуляции)!
 ?>
